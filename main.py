bc67c9e57c25fd16f0115b557c6ec54296fa90c4
aidacisse603-pixel <aidacisse603@gmail.com>
2026-05-08 11:37:58 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,48 @@
+import streamlit as st
+st.markdown("""
+<style>
+.stApp {
+    background-color: #E0E0E0;  /* gris clair */
+}
+</style>
+""", unsafe_allow_html=True)
+st.set_page_config(layout="wide")
+st.title("AIDA CISSE")
+   
+st.subheader("INGENIEUR EN AGRONOMIE-| AGROECOLOGISTE| AGROBUSENESS")
+
+st.write("________________________________________________________")
+
+with st.sidebar:
+     st.subheader("INFOS PERSONNELS")
+     st.image("WhatsApp Image 2025-09-29 à 01.22.45_4edf5f0b.jpg", width = 190)
+     st.write("📍:     Dakar, SENEGAL")
+     st.write("📧:    Email: cisse.aida@uam.edu.sn")
+
+     st.markdown("N'hésitez pas à me contacter par e-mail, je vous repondrai dans les plus bref delais.")
+
+st.subheader(" |A PROPOS DE MOI!")  
+st.markdown("Technicienne en Sciences et Techniques de l’Agriculture, de l’Alimentation et de la Nutrition, maîtrisant les bases de la production agricole, de l’alimentation et de la nutrition. Capable d’assurer le suivi des activités agricoles, de participer à des actions de sensibilisation nutritionnelle et de contribuer au contrôle de la qualité des produits alimentaires. Sérieuse, organisée et dynamique, avec une bonne capacité d’adaptation et un esprit d’équipe. Motivée à mettre ses compétences au service du développement agricole et de la sécurité alimentaire.")
+st.write("___________________________________________________________________________________")
+st.header("|FORMATIONS")
+st.subheader("DIPLOMES")
+st.write("2025/2026: LICENCE")
+st.write("2024/2025: BACCALAUREAT")
+st.header("|EXPERIENCES PROFESIONNELLES")
+st.subheader(" 18/09/2025 Stage en BAMBAYE dans un champ familial")
+st.markdown("Participation aux activités agricoles familiales dans les champs de mes grands-parents, incluant la récolte quotidienne de mil, d’arachide, de niébé et de feuilles de baobab, ainsi que leur transformation en produits alimentaires traditionnels . ")
+     
+st.subheader("|COMPETENCES ET LOGICIELS")
+st.markdown(" 2022-2023 Chargée de la commution et de l'information du club scientifique du Lycée de Thiaroyee")
+st.write("•	Capacité à travailler en équipe")
+st.write("•	Capacité à interagir efficacement avec les autres")
+st.write("•	Capacité d’analyse face aux problèmes et sens de l’observation")
+st.write("•	Compétences en communication : savoir trouver les mots justes et utiliser un langage approprié pour convaincre")
+st.write("•	Capacité d’écoute : être attentif, comprendre et répondre efficacement aux autres")
+st.write("•	Maîtrise de certains logiciels : Pack office")
+   
+
+
+
+
+        
